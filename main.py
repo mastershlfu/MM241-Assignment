39010d9b15303f8e1dcf5e5d4c323c36817635d6
Hà Bảo Nhi <baonhipct@gmail.com>
2024-12-05 09:58:22 +0700

    update lần 2

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import gym_cutting_stock
+import time
+#import timeit
 import gymnasium as gym
 from policy import GreedyPolicy, RandomPolicy
 from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
+
+# tạo biến thời gian
+start_time = time.time()
+print(f"Thời gian bắt đầu: {time.ctime(start_time)}")
 
 # Create the environment
 env = gym.make(
@@ -22,37 +28,58 @@ if __name__ == "__main__":
         observation, reward, terminated, truncated, info = env.step(action)
 
         if terminated or truncated:
-            observation, info = env.reset(seed=ep)
             print(info)
+            observation, info = env.reset(seed=ep)
             ep += 1
+            
+    # In thời gian kết thúc
+    end_time = time.time()
+    print(f"Thời gian kết thúc: {time.ctime(end_time)}")
+
+    # Tính toán thời gian trôi qua (elapse time)
+    elapse_time = end_time - start_time
+    print(f"Elapse time: {elapse_time:.6f} giây")
 
     # Reset the environment
-    observation, info = env.reset(seed=42)
+    # observation, info = env.reset(seed=42)
 
     # Test RandomPolicy
-    rd_policy = RandomPolicy()
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = rd_policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
+    # rd_policy = RandomPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = rd_policy.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
 
-        if terminated or truncated:
-            observation, info = env.reset(seed=ep)
-            print(info)
-            ep += 1
+    #     if terminated or truncated:
+    #         observation, info = env.reset(seed=ep)
+    #         print(info)
+    #         ep += 1
 
     # Uncomment the following code to test your policy
-    # # Reset the environment
+    # Reset the environment
     # observation, info = env.reset(seed=42)
-    # print(info)
 
     # policy2210xxx = Policy2210xxx()
+    # # code lấy từ phía trên 
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = policy2210xxx.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
+
+    #     if terminated or truncated:
+    #         print(info)
+    #         observation, info = env.reset(seed=ep)
+    #         #print(info)
+    #         ep += 1
+
+    # code ban đầu của thầy 
     # for _ in range(200):
     #     action = policy2210xxx.get_action(observation, info)
     #     observation, reward, terminated, truncated, info = env.step(action)
-    #     print(info)
 
     #     if terminated or truncated:
+    #         print(info)
     #         observation, info = env.reset()
 
 env.close()
+
